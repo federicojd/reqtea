64a37b24e20ea709c2b3debd350dc041e82f87b2
Federico Digiorgio <federicojd@icloud.com>
2026-07-04 22:38:14 -0300

    update viewport

--- internal/request/model.go
+++ internal/request/model.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 
 	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type Response struct {
@@ -19,13 +21,22 @@ type Model struct {
 	method  int
 	url     textinput.Model
 
-	response *Response
+	response         *Response
+	responseViewport viewport.Model
+	ready            bool
 }
 
 type responseMsg struct {
 	Response Response
 	Err      error
 }
+
+var panelStyle = lipgloss.NewStyle().
+	Border(lipgloss.RoundedBorder()).
+	Padding(1)
+
+var titleStyle = lipgloss.NewStyle().
+	Bold(true)
 
 func New() Model {
 	url := textinput.New()
@@ -34,10 +45,13 @@ func New() Model {
 	url.CharLimit = 300
 	url.Width = 80
 
+	vp := viewport.New(76, 45)
+
 	return Model{
-		methods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
-		method:  0,
-		url:     url,
+		methods:          []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		method:           0,
+		url:              url,
+		responseViewport: vp,
 	}
 }
 
@@ -67,6 +81,12 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 				m.method++
 			}
 		}
+	case tea.WindowSizeMsg:
+		leftWidth := msg.Width / 3
+		rightWidth := msg.Width - leftWidth - 6
+
+		m.responseViewport.Width = rightWidth - 4
+		m.responseViewport.Height = msg.Height - 8
 
 	case responseMsg:
 		if msg.Err != nil {
@@ -74,20 +94,28 @@ func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
 				StatusCode: 0,
 				Body:       msg.Err.Error(),
 			}
+
+			m.responseViewport.SetContent(msg.Err.Error())
 			return m, nil
 		}
 
 		m.response = &msg.Response
+		m.responseViewport.SetContent(msg.Response.Body)
 		return m, nil
 	}
 
 	m.url, cmd = m.url.Update(msg)
+
+	if m.response != nil {
+		m.responseViewport, cmd = m.responseViewport.Update(msg)
+	}
+
 	return m, cmd
 }
 
 func (m Model) View() string {
-	view := fmt.Sprintf(
-		`New Request
+	requestPanel := fmt.Sprintf(
+		`%s
 
 Method: %s
 
@@ -97,20 +125,36 @@ URL:
 ←/→ change method
 Ctrl+R send request
 Esc back
-`,
+	`,
+		titleStyle.Render("Request"),
 		m.methods[m.method],
 		m.url.View(),
 	)
 
+	responsePanel := titleStyle.Render("Response")
+
 	if m.response != nil {
-		view += fmt.Sprintf(
-			"\nStatus: %d\n\n%s",
+		responsePanel = fmt.Sprintf(
+			"\n%s\n\nStatus: %d\n\n%s\n",
+			titleStyle.Render("Response"),
 			m.response.StatusCode,
-			m.response.Body,
+			m.responseViewport.View(),
 		)
 	}
 
-	return view
+	left := panelStyle.
+		Width(50).
+		Render(requestPanel)
+
+	right := panelStyle.
+		Width(80).
+		Render(responsePanel)
+
+	return lipgloss.JoinHorizontal(
+		lipgloss.Top,
+		left,
+		right,
+	)
 }
 
 func (m Model) Method() string {
